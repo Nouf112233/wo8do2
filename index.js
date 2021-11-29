0929219de7df8e1cf3f7a8e8e04dbc685c0a5d02
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-11-29 16:43:55 +0300

    create index file

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
